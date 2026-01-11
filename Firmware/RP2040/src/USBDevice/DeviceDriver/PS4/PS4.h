bb510646ab94f032fef6acef5ab59b6c2e918018
pruebas555k-byte <pruebas555k@gmail.com>
2026-01-10 21:11:09 -0300

    Refactor PS4Device class in PS4.h
    Removed OutReport member and added array include.

--- Firmware/RP2040/src/USBDevice/DeviceDriver/PS4/PS4.h
+++ Firmware/RP2040/src/USBDevice/DeviceDriver/PS4/PS4.h
@@ -2,6 +2,7 @@
 #define _PS4_DEVICE_H_
 
 #include <cstdint>
+#include <array>
 
 #include "USBDevice/DeviceDriver/DeviceDriver.h"
 #include "Descriptors/PS4Device.h"
@@ -12,33 +13,25 @@ public:
     void initialize() override;
     void process(const uint8_t idx, Gamepad& gamepad) override;
 
-    uint16_t get_report_cb(uint8_t itf,
-                           uint8_t report_id,
+    uint16_t get_report_cb(uint8_t itf, uint8_t report_id,
                            hid_report_type_t report_type,
-                           uint8_t *buffer,
-                           uint16_t reqlen) override;
+                           uint8_t *buffer, uint16_t reqlen) override;
 
-    void set_report_cb(uint8_t itf,
-                       uint8_t report_id,
+    void set_report_cb(uint8_t itf, uint8_t report_id,
                        hid_report_type_t report_type,
-                       uint8_t const *buffer,
-                       uint16_t bufsize) override;
+                       uint8_t const *buffer, uint16_t bufsize) override;
 
-    bool vendor_control_xfer_cb(uint8_t rhport,
-                                uint8_t stage,
+    bool vendor_control_xfer_cb(uint8_t rhport, uint8_t stage,
                                 tusb_control_request_t const *request) override;
 
-    const uint16_t* get_descriptor_string_cb(uint8_t index,
-                                             uint16_t langid) override;
-
+    const uint16_t* get_descriptor_string_cb(uint8_t index, uint16_t langid) override;
     const uint8_t* get_descriptor_device_cb() override;
     const uint8_t* get_hid_descriptor_report_cb(uint8_t itf) override;
     const uint8_t* get_descriptor_configuration_cb(uint8_t index) override;
     const uint8_t* get_descriptor_device_qualifier_cb() override;
 
 private:
-    PS4_DEV::InReport  report_in_;
-    PS4_DEV::OutReport report_out_;
+    PS4Dev::InReport report_in_;
 };
 
 #endif // _PS4_DEVICE_H_
